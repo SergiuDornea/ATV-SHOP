11c9d93506090d0cfbcef9d75c70378e2cb5b4e6
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-04-04 09:57:24 +0300

    Add files via upload

---
+++ Registration.only/script.Registration.js
@@ -0,0 +1,39 @@
+//Contact script
+function openContact() {
+    document.getElementById("myContact").style.display = "block";
+}
+
+function closeContact() {
+    document.getElementById("myContact").style.display = "none";
+}
+
+// Stiky navbar
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() { myFunction() };
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+
+
+/* Open the Contact */
+function openCont() {
+    document.getElementById("myContact").style.width = "100%";
+}
+
+/* Close/hide the contact */
+function closeCont() {
+    document.getElementById("myContact").style.width = "0";
+}
